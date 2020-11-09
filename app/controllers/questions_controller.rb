45cb275fee02616a409b8481131c0d30fbc7f8f0
thomasfarre <th.farre@gmail.com>
2020-11-09 12:18:28 +0100

    exo2

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params[:answer].end_with?("?")
+      @result = "Silly question, get dressed and go to work!"
+    elsif params[:answer].downcase == "i am going to work"
+      @result = "Great!"
+    else
+      @result = "I don't care, get dressed and go to work!"
+    end
+  end
+end
